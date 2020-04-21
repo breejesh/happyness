4cc324bd1b9b5be247ec25343f5040fc42ecff39
himanshuMaheshwari2311 <m.himanshu2311@gmail.com>
2020-04-21 23:24:09 +0530

    Before debug cloud_firestore

---
+++ lib/screens/SigninScreen.dart
@@ -0,0 +1,92 @@
+import 'package:flutter/material.dart';
+import 'package:happyness/screens/NewsScreen.dart';
+import 'package:happyness/service/Authentication.dart';
+
+class SigninScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder(
+        stream: authenticationService.user,
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            return NewsScreen();
+          } else {
+            return Container(
+              child: Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    RaisedButton(
+                        onPressed: () => authenticationService.googleSignIn(),
+                        padding:
+                            EdgeInsets.only(top: 3.0, bottom: 3.0, left: 3.0),
+                        color: const Color(0xFFFFFFFF),
+                        child: new Row(
+                          mainAxisSize: MainAxisSize.min,
+                          children: <Widget>[
+                            new Image.asset(
+                              'assets/images/google_button.jpg',
+                              height: 40.0,
+                            ),
+                            new Container(
+                                padding:
+                                    EdgeInsets.only(left: 10.0, right: 10.0),
+                                child: new Text(
+                                  "Sign in with Google",
+                                  style: TextStyle(
+                                      color: Colors.grey,
+                                      fontWeight: FontWeight.bold),
+                                )),
+                          ],
+                        )),
+                  ],
+                ),
+              ),
+            );
+          }
+        });
+  }
+}
+
+// class SigninScreen extends StatefulWidget {
+//   // LoginSignupScree({this.})
+
+//   // final BaseAuth
+//   @override
+//   _SigninScreenState createState() => _SigninScreenState();
+// }
+
+// class _SigninScreenState extends State<SigninScreen> {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Container(
+//       child: Center(
+//         child: Column(
+//           mainAxisAlignment: MainAxisAlignment.center,
+//           children: <Widget>[
+//             RaisedButton(
+//                 onPressed: () => authenticationService.googleSignIn(),
+//                 padding: EdgeInsets.only(top: 3.0, bottom: 3.0, left: 3.0),
+//                 color: const Color(0xFFFFFFFF),
+//                 child: new Row(
+//                   mainAxisSize: MainAxisSize.min,
+//                   children: <Widget>[
+//                     new Image.asset(
+//                       'asset/google_button.jpg',
+//                       height: 40.0,
+//                     ),
+//                     new Container(
+//                         padding: EdgeInsets.only(left: 10.0, right: 10.0),
+//                         child: new Text(
+//                           "Sign in with Google",
+//                           style: TextStyle(
+//                               color: Colors.grey, fontWeight: FontWeight.bold),
+//                         )),
+//                   ],
+//                 )),
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+// }
---
+++ lib/service/Authentication.dart
@@ -0,0 +1,66 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:rxdart/rxdart.dart';
+
+class AuthenticationService {
+  final GoogleSignIn _googleSignIn = GoogleSignIn();
+  final FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
+  final Firestore _db = Firestore.instance;
+
+  Observable<FirebaseUser> user;
+  Observable<Map<String, dynamic>> profile;
+  PublishSubject loading = PublishSubject();
+
+  AuthenticationService() {
+    user = Observable(_firebaseAuth.onAuthStateChanged);
+
+    profile = user.switchMap((FirebaseUser user) {
+      if (user != null) {
+        return _db
+            .collection('users')
+            .document(user.uid)
+            .snapshots()
+            .map((snap) => snap.data);
+      } else {
+        return Observable.just({});
+      }
+    });
+  }
+
+  Future<FirebaseUser> googleSignIn() async {
+    loading.add(true);
+
+    GoogleSignInAccount googleSignInAccount = await _googleSignIn.signIn();
+
+    GoogleSignInAuthentication googleSignInAuthentication =
+        await googleSignInAccount.authentication;
+
+    FirebaseUser user = await _firebaseAuth.signInWithGoogle(
+        idToken: googleSignInAuthentication.idToken,
+        accessToken: googleSignInAuthentication.accessToken);
+
+    updateUserData(user);
+    loading.add(false);
+    print("signed in as:" + user.displayName);
+    return user;
+  }
+
+  void updateUserData(FirebaseUser user) async {
+    DocumentReference ref = _db.collection('users').document(user.uid);
+
+    return ref.setData({
+      'uid': user.uid,
+      'email': user.email,
+      'photoURL': user.photoUrl,
+      'displayName': user.displayName,
+      'lastSeen': DateTime.now()
+    }, merge: true);
+  }
+
+  void signOut() {
+    _firebaseAuth.signOut();
+  }
+}
+
+final AuthenticationService authenticationService = AuthenticationService();
