c9e46f031153ef94352424ac0b5eb20818ea0bdd
sagarkamdar <sagarkamdar37@gmail.com>
2020-04-22 23:16:42 +0530

    Floating share button

--- lib/screens/NewsScreen.dart
+++ lib/screens/NewsScreen.dart
@@ -1,7 +1,11 @@
 import 'dart:developer';
+import 'dart:typed_data';
+import 'dart:ui' as ui;
 
+import 'package:esys_flutter_share/esys_flutter_share.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_swiper/flutter_swiper.dart';
 import 'package:happyness/data/NewsArticle.dart';
 import 'package:happyness/widgets/NewsWrapperWidget.dart';
@@ -12,31 +16,56 @@ class NewsScreen extends StatefulWidget {
 }
 
 class _NewsScreenState extends State<NewsScreen> {
-  int _currentIndex = 0;
+  static GlobalKey scaffoldKey = new GlobalKey();
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      resizeToAvoidBottomInset: false, // Fix for keyboard resizing issue - not sure how viable when we want to actually use keyboard
-      backgroundColor: Colors.black,
-      body: SafeArea(
+    return RepaintBoundary(
+      key: scaffoldKey,
+      child: Scaffold(
+        floatingActionButton: FloatingActionButton(
+          child: Icon(Icons.share),
+          onPressed: onShare,
+        ),
+        resizeToAvoidBottomInset:
+            false, // Fix for keyboard resizing issue - not sure how viable when we want to actually use keyboard
+        backgroundColor: Colors.black,
+        body: SafeArea(
           child: Swiper(
-        onIndexChanged: onIndexChanged,
-        itemBuilder: (BuildContext context, int index) {
-          return new NewsWrapperWidget(newsArticles[index]);
-        },
-        loop: false,
-        scrollDirection: Axis.vertical,
-        itemWidth: MediaQuery.of(context).size.width,
-        itemHeight: MediaQuery.of(context).size.height,
-        layout: SwiperLayout.STACK,
-        itemCount: newsArticles.length,
-      )),
+            index: 3,
+            itemBuilder: (BuildContext context, int index) {
+              return new NewsWrapperWidget(newsArticles[index]);
+            },
+            loop: false,
+            scrollDirection: Axis.vertical,
+            itemWidth: MediaQuery.of(context).size.width,
+            itemHeight: MediaQuery.of(context).size.height,
+            layout: SwiperLayout.STACK,
+            itemCount: newsArticles.length,
+          ),
+        ),
+      ),
     );
   }
 
-  void onIndexChanged(int index) {
-    _currentIndex = index;
-    log('onIndexChanged index: $index');
+  Future<void> onShare() async {
+    log('onShare: ');
+    await takeScreenShotAndShare();
+  }
+
+  Future<void> takeScreenShotAndShare() async {
+    try {
+      RenderRepaintBoundary boundary =
+          scaffoldKey.currentContext.findRenderObject();
+      ui.Image image = await boundary.toImage();
+      ByteData byteData =
+          await image.toByteData(format: ui.ImageByteFormat.png);
+      Uint8List pngBytes = byteData.buffer.asUint8List();
+      await Share.file('Share a screenshot of Happyness!', 'goodnews.png',
+          pngBytes, 'image/png',
+          text: 'Download Happyness for more');
+    } catch (e) {
+      print('error: $e');
+    }
   }
 }
