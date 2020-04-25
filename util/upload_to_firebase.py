56b7b49543355415f1285c8dd8a9a7f7e2580f20
himanshuMaheshwari2311 <m.himanshu2311@gmail.com>
2020-04-25 20:12:11 +0530

    Modifying upload util and fixing pubspec.lock

--- util/upload_to_firebase.py
+++ util/upload_to_firebase.py
@@ -1,11 +1,12 @@
 from firebase_admin import credentials, firestore, initialize_app
 import csv
+import hashlib
 
 db = None
 
 def init_firebase():
     global db
-    cred = credentials.Certificate('./happyness-firebase.json')
+    cred = credentials.Certificate('./happyness-python-firebase.json')
     app = initialize_app(cred)
     db = firestore.client(app)
 
@@ -14,6 +15,8 @@ def upload_content_all():
         csv_reader = csv.DictReader(file, quoting=csv.QUOTE_ALL, skipinitialspace=True)
         for row in csv_reader:
             news_json = dict(row)
+            news_json['id'] = hashlib.md5(news_json['title'].encode()).hexdigest()
+            print (news_json)
             news_ref = db.collection('news')
             news_ref.document(news_json['id']).set(news_json)
 
